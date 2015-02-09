fa805c943a69f93dc556ea0b51fdd15d68968214
Ian Horn <horn.imh@gmail.com>
2015-02-09 14:00:14 -0800

    Added mean computed from maps to example test

--- tests/test.py
+++ tests/test.py
@@ -5,6 +5,15 @@ from gnss_analysis.abstract_analysis.analysis import *
 from gnss_analysis.abstract_analysis.report import *
 
 
+#A map that just holds the initial data
+class DatumA(Analysis):
+  def __init__(self):
+    super(DatumA, self).__init__(
+      key='data',
+      keep_as_map=True)
+  def compute(self, datum, current_analyses, prev_fold):
+    return datum
+
 #A fold to compute the sum of squares of the data points
 class SumSquareA(Analysis):
   def __init__(self, keep_as_map=False):
@@ -38,7 +47,7 @@ class CountA(Analysis):
   def compute(self, datum, current_analyses, prev_fold):
     return prev_fold['count'] + 1
 
-#A summary to compute the mean
+#A summary to compute the mean from folds
 class MeanA(Analysis):
   def __init__(self):
     super(MeanA, self).__init__(
@@ -48,7 +57,7 @@ class MeanA(Analysis):
   def compute(self, data, current_analyses, final_fold):
     return float(final_fold['sum']) / float(final_fold['count'])
 
-#A report for the data mean
+#A report for the data mean from folds
 class MeanR(Report):
   def __init__(self):
     super(MeanR, self).__init__(
@@ -56,6 +65,26 @@ class MeanR(Report):
       parents=set([MeanA()]))
   def report(self, data, analyses, folds):
     return str(analyses['mean'])
+
+#A summary to compute the mean from maps
+class MeanA2(Analysis):
+  def __init__(self):
+    super(MeanA2, self).__init__(
+      key='mean2',
+      parents=set([DatumA()]),
+      is_summary=True)
+  def compute(self, data, current_analyses, folds):
+    print current_analyses['data'].mean()
+    return current_analyses['data'].mean()
+
+#A report for the data mean from maps
+class MeanR2(Report):
+  def __init__(self):
+    super(MeanR2, self).__init__(
+      key='mean2',
+      parents=set([MeanA2()]))
+  def report(self, data, analyses, folds):
+    return str(analyses['mean2'])
 
 #A summary to compute the variance of the data
 class VarianceA(Analysis):
@@ -84,6 +113,7 @@ if __name__ == '__main__':
   tester = SITL(update_function, data)
   tester.add_report(MeanR())
   tester.add_report(VarianceR())
+  tester.add_report(MeanR2())
   reports = tester.compute()
   for report in reports.iteritems():
     print '(key=' + report[0] + ') \t' + str(report[1])
