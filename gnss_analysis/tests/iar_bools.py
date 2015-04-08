10392c6a1f23c7df481b232d2c86e687375e8429
Ian Horn <horn.imh@gmail.com>
2015-04-08 16:42:08 -0700

    Added tests for the log likelihood and chi square of ILS solutions

--- gnss_analysis/tests/iar_bools.py
+++ gnss_analysis/tests/iar_bools.py
@@ -12,8 +12,10 @@
 from gnss_analysis.abstract_analysis.analysis import *
 from gnss_analysis.abstract_analysis.report import *
 import gnss_analysis.utils as ut
+from gnss_analysis.tests.kf_internals import *
 import sbp_log_analysis.metrics_schema as ms
 import swiftnav.dgnss_management as mgmt
+import numpy as np
 
 # Is the actual IA vector in the set of hyps initially generated?
 # Is the actual IA vector the one found at the end?
@@ -55,7 +57,7 @@ class FixedIARBegun(Analysis):
   def compute(self, data, current_analyses, prev_fold, parameters):
     if prev_fold['FixedIARBegun']:
       return True
-    return mgmt.dgnss_iar_num_sats() > 0 and mgmt.dgnss_iar_num_hyps() > 0
+    return iar_going()
 
 class FixedIARCompleted(Analysis):
   """
@@ -72,6 +74,107 @@ class FixedIARCompleted(Analysis):
     if prev_fold['FixedIARCompleted']:
       return True
     return current_analyses['FixedIARBegun'] and mgmt.dgnss_iar_resolved()
+
+def amb_test_can_do_ils():
+  return mgmt.dgnss_iar_num_sats() >= 4  and mgmt.dgnss_iar_num_hyps() > 0
+
+class FixedILSVector(Analysis):
+  def __init__(self):
+    super(FixedILSVector, self).__init__(
+      key='FixedILSVector',
+      keep_as_map=True)
+  def compute(self, data, current_analyses, prev_fold, parameters):
+    if amb_test_can_do_ils():
+      dat = data.apply(ut.mk_swiftnav_sdiff, axis=0).dropna()
+      fxd_de, fxd_phase = mgmt.get_iar_de_and_phase(
+        dat, parameters.local_ecef + 0.5 * parameters.known_baseline)
+      ia_vec_from_b = ut.get_N_from_b(fxd_phase,
+                                      fxd_de,
+                                      parameters.known_baseline)
+      return ia_vec_from_b
+    else:
+      return None
+
+class FixedILSVectorR(Report):
+  def __init__(self):
+    super(FixedILSVectorR, self).__init__(
+      key='FixedILSVector',
+      parents=set([FixedILSVector()]))
+  def report(self, data, analyses, folds, parameters):
+    return analyses['FixedILSVector']
+
+def kf_can_do_ils():
+  return mgmt.get_sats_management_num_sats() >= 4
+
+class FixedILSVectorLL(Analysis):
+  def __init__(self):
+    super(FixedILSVectorLL, self).__init__(
+      key='FixedILSVectorLL',
+      parents=set([FixedILSVector()]),
+      keep_as_map=True)
+  def compute(self, data, current_analyses, prev_fold, parameters):
+    ia_vec = current_analyses['FixedILSVector']
+    if ia_vec is None:
+      return np.nan
+    return mgmt.dgnss_iar_pool_ll(ia_vec)
+
+class FixedILSVectorLLR(Report):
+  def __init__(self):
+    super(FixedILSVectorLLR, self).__init__(
+      key='FixedILSVectorLL',
+      parents=set([FixedILSVectorLL()]))
+  def report(self, data, analyses, folds, parameters):
+    return analyses['FixedILSVectorLL']
+
+class FloatILSVector(Analysis):
+  def __init__(self):
+    super(FloatILSVector, self).__init__(
+      key='FloatILSVector',
+      keep_as_map=True)
+  def compute(self, data, current_analyses, prev_fold, parameters):
+    if kf_can_do_ils():
+      dat = data.apply(ut.mk_swiftnav_sdiff, axis=0).dropna()
+      flt_de, flt_phase = mgmt.get_float_de_and_phase(
+        dat, parameters.local_ecef + 0.5 * parameters.known_baseline)
+      ia_vec_from_b = ut.get_N_from_b(flt_phase,
+                                      flt_de,
+                                      parameters.known_baseline)
+      return ia_vec_from_b
+    else:
+      return None
+
+class FloatILSVectorR(Report):
+  def __init__(self):
+    super(FloatILSVectorR, self).__init__(
+      key='FloatILSVector',
+      parents=set([FloatILSVector()]))
+  def report(self, data, analyses, folds, parameters):
+    return analyses['FloatILSVector']
+
+class FloatILSVectorChisq(Analysis):
+  def __init__(self):
+    super(FloatILSVectorChisq, self).__init__(
+      key='FloatILSVectorChisq',
+      parents=set([FloatILSVector(),
+                   KFMean(),
+                   KFCov()]),
+      keep_as_map=True)
+  def compute(self, data, current_analyses, prev_fold, parameters):
+    ia_vec = current_analyses['FloatILSVector']
+    if ia_vec is None:
+      return np.nan, 0
+    ia_mean = current_analyses['KFMean']
+    ia_cov = current_analyses['KFCov']
+    dx = ia_vec - ia_mean
+    return dx.dot(np.linalg.lstsq(ia_cov, dx)[0]), len(ia_vec)
+
+class FloatILSVectorChisqR(Report):
+  def __init__(self):
+    super(FloatILSVectorChisqR, self).__init__(
+      key='FloatILSVectorChisq',
+      parents=set([FloatILSVectorChisq()]))
+  def report(self, data, analyses, folds, parameters):
+    return analyses['FloatILSVectorChisq']
 
 class FixedIARLeastSquareInPool(Analysis):
   def __init__(self):
