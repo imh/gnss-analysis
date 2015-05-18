37461f15ab4c4fe3a0bcf4bec89d75d5f05f5930
Bhaskar Mookerji <mookerji@gmail.com>
2015-05-18 12:53:56 -0700

    Misc. cleanup.
    - Removed some dead code mistakenly added earlier.
    - Table keys validation function used by some dependent commits.
    - Removed dangling __init__.py.
    
    /cc @imh

--- gnss_analysis/utils.py
+++ gnss_analysis/utils.py
@@ -112,40 +112,18 @@ def not_nan(x):
 def get_non_nans(xs):
   return xs[xs.apply(not_nan)]
 
-def truthifyv(phiv):
-    runs = []
-    current_run = []
-    current_low = np.nan
-    truth = np.empty(phiv.shape)
-    truth[:] = np.nan
-    for i, p in enumerate(phiv):
-        if np.isnan(p):
-            #then either just finished a run, or in the middle of a lull
-            if not np.isnan(current_low):
-                #then just finished a run
-                runs.append((current_low, i, np.array(current_run)))
-                current_low = np.nan
-                current_run = []
-        else:
-            #then just starting a run or in the middle of one
-            if np.isnan(current_low):
-                #then just starting a run
-                current_low = i
-                current_run.append(p)
-            else:
-                #then in the middle of a run
-                current_run.append(p)
-    #finally may have ended in a run
-    if not np.isnan(current_low):
-        runs.append((current_low, i+1, np.array(current_run)))
-    for low, high, run in runs:
-        truth[low:high] = round(np.median(run))
-    return truth
 
-def truthify(phi):
-    phiT = phi.values.T
-    truth = np.empty(phiT.shape)
-    truth[:,:] = np.nan
-    for i in range(len(phiT)):
-        truth[i] = truthifyv(phiT[i])
-    return pd.DataFrame(truth.T,index=phi.index, columns=phi.columns)
+def to_repr(obj):
+  """Returns string representation of an object.
+
+  """
+  items = ("%s = %r" % (k, v) for k, v in obj.__dict__.items())
+  return "<%s: {%s}>" % (obj.__class__.__name__, ', '.join(items))
+
+
+def validate_table_schema(coll, required_keys):
+  """Validates that the collection (intended: HDF5 file) has the
+  required keys.
+
+  """
+  return set(required_keys).issubset(set(coll.keys()))
