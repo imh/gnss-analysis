5eaa07537520c142514c314b693f92491e541ffb
Bhaskar Mookerji <mookerji@gmail.com>
2015-07-15 12:39:10 -0700

    Updates. KML stuff.

--- gnss_analysis/hitl_table_utils.py
+++ gnss_analysis/hitl_table_utils.py
@@ -504,6 +504,48 @@ def plot_pos_parametric(log):
   axs[1].hlines(y=0, xmin=fixed.min()['e'], xmax=fixed.max()['e'], color='black')
 
 
+def get_kml_doc(llhs):
+  """Generates a KML document from a Pandas table of single point
+  solutions. Requires columns lat, lon, and height.
+
+  """
+  from pykml.parser import Schema
+  from pykml.factory import KML_ElementMaker as KML
+  from pykml.factory import GX_ElementMaker as GX
+  center = llhs[['lat', 'lon', 'height']].mean()
+  elts = lambda e: '%s,%s,%d' % (e['lon'], e['lat'], e['height'])
+  coords = ' '.join(llhs.apply(elts, axis=1).values)
+  xml_coords = KML.coordinates(coords)
+  doc = KML.kml(
+          KML.Placemark(
+            KML.name("gx:altitudeMode Example"),
+            KML.LookAt(
+              KML.longitude(center.lon),
+              KML.latitude(center.lat),
+              KML.heading(center.height),
+              KML.tilt(70),
+              KML.range(6300),
+              GX.altitudeMode("relativeToSeaFloor"),),
+            KML.LineString(
+              KML.extrude(1),
+              GX.altitudeMode("relativeToSeaFloor"),
+              xml_coords
+            )
+          )
+        )
+  return doc
+
+def write_kml(llhs, fname='file.kml'):
+  """Generates and writes out a KML document from a Pandas table of
+  single point solutions. Requires lat, lon, and height.
+
+  """
+  from lxml import etree
+  doc = get_kml_doc(llhs)
+  doc_str = etree.tostring(doc, pretty_print=True)
+  with open(fname,'w+') as f:
+    f.write(doc_str)
+
 def build_panel(rover_log, k, gpst_key='approx_gps_time'):
   buf = {}
   for item in rover_log.rover_thread_state.items:
